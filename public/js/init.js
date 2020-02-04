78dc83e3a81e56e72a62a1d3fd1ea1f577807196
bez <arapi.besmir@gmail.com>
2020-02-04 17:14:35 +0100

    ADD file

---
+++ public/js/init.js
@@ -0,0 +1,8 @@
+
+function init() {
+    scheduler.config.xml_date = "%Y-%m-%d %H:%i";
+    scheduler.init('scheduler_here', new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()), "month");
+
+    scheduler.templates.xml_date = function (value) { return new Date(value); };
+    scheduler.load("/data", "json");
+}
